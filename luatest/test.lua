bfe18f0a29b42559a1e846c23befa5eb7ea8dc23
Reed Weichler <rweichler@gmail.com>
2013-09-29 01:20:59 -0700

    removed tabs EWWWWW sublime text

--- luatest/test.lua
+++ luatest/test.lua
@@ -5,11 +5,11 @@ function f(num)
     local result = ""
     local i = 0
     for k,v in next, d do
-    	result = result..v..", "
-    	i = i + 1
-    	if i == num then
-    		break
-    	end
+        result = result..v..", "
+        i = i + 1
+        if i == num then
+            break
+        end
     end
     return result
 end
