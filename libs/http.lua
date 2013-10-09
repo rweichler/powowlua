34e89cad2d27e59160343f3693ca718401064266
Reed Weichler <rweichler@gmail.com>
2013-10-08 21:06:05 -0700

    add libs

---
+++ libs/http.lua
@@ -0,0 +1,42 @@
+
+local http = {}
+
+local session = {}
+http.session = session
+
+function session:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    
+    o.cookies = {}
+    o.headers = {}
+
+    return o
+end
+
+function session:post(...)
+    return self:request("POST", ...)
+end
+
+function session:get(...)
+    return self:request("GET", ...)
+end
+
+function session:head(...)
+    return self:request("HEAD", ...)
+end
+
+function session:put(...)
+    return self:request("PUT", ...)
+end
+
+function session:patch(...)
+    return self:request("PATCH", ...)
+end
+
+function session:delete(...)
+    return self:request("DELETE", ...)
+end
+
+return http
